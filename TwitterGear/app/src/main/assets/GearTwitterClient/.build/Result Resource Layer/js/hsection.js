7531bbc02bb1b5a87cb79996f7d3d8a3187ab41b
Saul M <saulmm2@gmail.com>
2015-01-21 03:39:37 +0100

    Refactored testTweets by rawTweets and testTweetParts to rawTweetParts

--- TwitterGear/app/src/main/assets/GearTwitterClient/.build/Result Resource Layer/js/hsection.js
+++ TwitterGear/app/src/main/assets/GearTwitterClient/.build/Result Resource Layer/js/hsection.js
@@ -8,39 +8,34 @@ var page = document.getElementById( "hsectionchangerPage" ),
 
 		tweetsContainer = document.getElementById('tweets_container');
 
-		var testTweets = window.localStorage.getItem("tweets");
-		var tweets = testTweets.split ("|_|");
-		console.log("Test tweets: "+testTweets);
+		var rawTweets = window.localStorage.getItem("tweets");
+		var tweets = rawTweets.split ("|_|");
 
 		for (var i = 0; i < tweets.length; i++) {
 
-			var testTweetParts = tweets[i].split("-.__");
+			var rawTweetParts = tweets[i].split("-.__");
 			
 			var nameElement = document.createElement("p");
-			nameElement.innerHTML = testTweetParts[0];
+			nameElement.innerHTML = rawTweetParts[0];
 			nameElement.className = "tweetName"
 
 			var timeElement = document.createElement("p");
-			timeElement.innerHTML = testTweetParts[1];
+			timeElement.innerHTML = rawTweetParts[1];
 			timeElement.className = "tweetTime"
 
 			var usernameElement = document.createElement("p");
-			usernameElement.innerHTML = testTweetParts[2];
+			usernameElement.innerHTML = rawTweetParts[2];
 			usernameElement.className = "tweetUsername"
 
 			var tweetElement = document.createElement("p");
 
-			testTweetParts[3] = testTweetParts[3].replace(/@(\w+)/g,"<font color='85CBFF'>@$1</font>")
-			testTweetParts[3] = testTweetParts[3].replace(/#(\w+)/g,"<font color='85CBFF'>@$1</font>")
-			testTweetParts[3] = testTweetParts[3].replace(/(\(.*?)?\b((?:https?|ftp|file):\/\/[-a-z0-9+&@#\/%?=~_()|!:,.;]*[-a-z0-9+&@#\/%=~_()|])/ig,
+			rawTweetParts[3] = rawTweetParts[3].replace(/@(\w+)/g,"<font color='85CBFF'>@$1</font>")
+			rawTweetParts[3] = rawTweetParts[3].replace(/#(\w+)/g,"<font color='85CBFF'>#$1</font>")
+			rawTweetParts[3] = rawTweetParts[3].replace(/(\(.*?)?\b((?:https?|ftp|file):\/\/[-a-z0-9+&@#\/%?=~_()|!:,.;]*[-a-z0-9+&@#\/%=~_()|])/ig,
 			"");
-
-		
 			
-			
-			tweetElement.innerHTML = testTweetParts[3];
+			tweetElement.innerHTML = rawTweetParts[3];
 			tweetElement.className = "tweetText"
-
 
 			// Icon of favorite
 			var favoriteImage = document.createElement ("img");
@@ -50,15 +45,16 @@ var page = document.getElementById( "hsectionchangerPage" ),
 
 			// Text with rhe favorite count
 			var favoriteText = document.createElement ("p");
+			favoriteText.setAttribute ("id", "fav_text");
 			favoriteText.setAttribute ("class", "tweet_action_text");
-			favoriteText.innerHTML = ""+testTweetParts[5];
+			favoriteText.innerHTML = ""+rawTweetParts[5];
 			
 			// Container to wrap the favorite elements
 			var favoriteContainer = document.createElement ("div");
 			favoriteContainer.setAttribute ("class", "tweet_action_container");
 			favoriteContainer.appendChild (favoriteImage);
 			favoriteContainer.appendChild (favoriteText);
-			favoriteContainer.setAttribute ("onclick", "favorite(\""+testTweetParts[4]+"\")");
+			favoriteContainer.setAttribute ("onclick", "favorite(\""+rawTweetParts[4]+"\")");
 
 			// Icon of the retweet
 			var retweetImage = document.createElement ("img");
@@ -68,15 +64,16 @@ var page = document.getElementById( "hsectionchangerPage" ),
 			
 			// Text with rhe retweet count
 			var retweetText = document.createElement ("p");
+			retweetText.setAttribute ("id", "rt_text");
 			retweetText.setAttribute ("class", "tweet_action_text");
-			retweetText.innerHTML = ""+testTweetParts[6];
+			retweetText.innerHTML = ""+rawTweetParts[6];
 			
 			// Container to wrap the retweet elements
 			var retweetContainer = document.createElement ("div");
 			retweetContainer.setAttribute ("class", "tweet_action_container");
 			retweetContainer.appendChild (retweetImage);
 			retweetContainer.appendChild (retweetText);
-			retweetContainer.setAttribute ("onclick", "favorite(\""+testTweetParts[4]+"\")");
+			retweetContainer.setAttribute ("onclick", "retweet(\""+rawTweetParts[4]+"\")");
 		
 			// The container that align both containers as a row
 			var actionsContainer = document.createElement("div");
